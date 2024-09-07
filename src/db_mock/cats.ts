075e99b387b44800a8cefa06537e5fac6cee9c4e
Federico Benavidez <fedoteh@gmail.com>
2024-09-07 19:44:26 -0300

    Add mock data

---
+++ src/db_mock/cats.ts
@@ -0,0 +1,31 @@
+const cats = [
+    {
+        id: 1,
+        name: "Sherlock",
+        lastname: "Holmes",
+        sex: "Male",
+        breed: "American Shorthair",
+        color: ["White", "Orange"],
+        weight: 8.2
+    },
+    {
+        id: 2,
+        name: "Irene",
+        lastname: "Adler",
+        sex: "Female",
+        breed: "Bombay",
+        color: ["Black"],
+        weight: 4.7
+    },
+    {
+        id: 3,
+        name: "Mr. Mittens",
+        lastname: "Turbofreckles",
+        sex: "Male",
+        breed: "Maine Coon",
+        color: ["Gray"],
+        weight: 7.1
+    }
+];
+
+export default cats;
---
+++ src/db_mock/dogs.ts
@@ -0,0 +1,32 @@
+const dogs = [
+    {
+        id: 1,
+        name: "Leia",
+        lastname: "Organa",
+        sex: "Female",
+        breed: "Golden Retriever",
+        color: ["Golden"],
+        weight: 42
+    },
+    {
+        id: 2,
+        name: "Totoño",
+        lastname: "Leopoldo",
+        sex: "Male",
+        breed: null,
+        color: ["Light Yellow"],
+        weight: 10
+    },
+    {
+        id: 3,
+        name: "Guauguaucito",
+        lastname: "Feroz",
+        sex: "Female",
+        breed: "Jack Russel",
+        color: ["White", "Brown"],
+        weight: 15
+    },
+
+];
+
+export default dogs;
