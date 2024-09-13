4af547f0a9cd6de3222d751d2536474f0570249f
Federico Benavidez <fedoteh@gmail.com>
2024-09-12 21:33:03 -0300

    Fix secrets syntax in docker-compose

--- src/routes/pets/cats/catsRouter.ts
+++ src/routes/pets/cats/catsRouter.ts
@@ -2,14 +2,21 @@ import express, { NextFunction, Request, Response } from "express";
 import cats from "../../../db_mock/cats";
 import { findPetById } from "../../../util/findPet";
 import { Pet } from "../../../util/types/types";
+import pool  from "../../../util/db/pool";
 
 // Create a new router to handle the cats routes
 const catsRouter = express.Router();
 
 // Get all cats
-catsRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
-    const catsList = cats; // Get the list of cats from the mock database
-    res.status(200).send(catsList);
+catsRouter.get('/', async (req: Request, res: Response, next: NextFunction) => {
+    //const catsList = cats; // Get the list of cats from the mock database
+    try {
+        const result = await pool.query('SELECT NOW()');
+        res.json(result.rows);
+      } catch (err) {
+        console.error('Error executing query', (err as Error).stack);
+        res.status(500).send('Server error');
+      }
 });
 
 // Get a specific cat
