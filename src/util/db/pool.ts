ac48f706df4c81232e14a0477d344a86bf3b6eb0
Federico Benavidez <fedoteh@gmail.com>
2024-09-14 19:34:25 -0300

    Removed the connection test from the pool

--- src/util/db/pool.ts
+++ src/util/db/pool.ts
@@ -6,25 +6,6 @@ const pool = new Pool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
     port: parseInt(process.env.DB_PORT || '5432', 10),
-    });
-  
-
-      
-pool.connect((err, client, release) => {
-    if (err) {
-        console.error('Error acquiring client', err.stack);
-        return;
-    }
-    if (client) {       
-        client.query('SELECT NOW()', (err, result) => {         
-        release();
-        if (err) {
-            console.error('Error executing query', err.stack);
-            return;
-        }
-        console.log(result.rows);
-        });
-    }
-});   
+});
 
 export default pool;
