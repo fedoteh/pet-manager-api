fec28088bbedda931839547f18c19ac1d064b990
Federico Benavidez <fedoteh@gmail.com>
2024-09-14 19:39:41 -0300

    Initial setup for db tables creation

---
+++ src/util/db/setup.sql
@@ -0,0 +1,25 @@
+CREATE TABLE IF NOT EXISTS dogs (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  lastname VARCHAR(255) NOT NULL,
+  sex VARCHAR(6) CHECK (sex IN ('male', 'female')) NOT NULL,
+  breed VARCHAR(255),
+  color VARCHAR(255)[] NOT NULL,
+  weight FLOAT NOT NULL,
+  profilePicture VARCHAR(255) NOT NULL,
+  activities VARCHAR(255)[],
+  favoriteSnack VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS cats (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  lastname VARCHAR(255) NOT NULL,
+  sex VARCHAR(6) CHECK (sex IN ('male', 'female')) NOT NULL,
+  breed VARCHAR(255),
+  color VARCHAR(255)[] NOT NULL,
+  weight FLOAT NOT NULL,
+  profilePicture VARCHAR(255) NOT NULL,
+  favoriteToy VARCHAR(255),
+  favoritePlace VARCHAR(255)
+);
