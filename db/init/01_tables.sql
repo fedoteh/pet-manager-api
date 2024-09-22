65eee6e15e31bb8914cc1ff45cbf398f7d0d8b2f
Federico Benavidez <fedoteh@gmail.com>
2024-09-22 20:04:58 -0300

    De-duplicated data by creating pets instead of two tables

--- db/init/01_tables.sql
+++ db/init/01_tables.sql
@@ -1,25 +1,21 @@
-CREATE TABLE IF NOT EXISTS dogs (
+CREATE TABLE IF NOT EXISTS species (
+  id SERIAL PRIMARY KEY,
+  name varchar(20),
+  description varchar(255)
+);
+
+CREATE TABLE IF NOT EXISTS pets (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
   lastname VARCHAR(255) NOT NULL,
+  species_id INT REFERENCES species(id) NOT NULL,  -- Foreign key referencing species table
   sex VARCHAR(6) CHECK (sex IN ('male', 'female')) NOT NULL,
   breed VARCHAR(255),
   color VARCHAR(255)[] NOT NULL,
   weight FLOAT NOT NULL,
   profilePicture VARCHAR(255),
   activities VARCHAR(255)[],
-  favoriteSnack VARCHAR(255)
-);
-
-CREATE TABLE IF NOT EXISTS cats (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR(255) NOT NULL,
-  lastname VARCHAR(255) NOT NULL,
-  sex VARCHAR(6) CHECK (sex IN ('male', 'female')) NOT NULL,
-  breed VARCHAR(255),
-  color VARCHAR(255)[] NOT NULL,
-  weight FLOAT NOT NULL,
-  profilePicture VARCHAR(255),
+  favoriteSnack VARCHAR(255),
   favoriteToy VARCHAR(255),
   favoritePlace VARCHAR(255)
 );
--- db/init/02_startingValues.sql
+++ db/init/02_startingValues.sql
@@ -1,15 +1,20 @@
--- Insert initial data into dogs table
-INSERT INTO dogs (id, name, lastname, sex, breed, color, weight)
+-- Insert the species our app manages
+INSERT INTO species (id, name, description) VALUES
+(1, 'Dog', 'Canis lupus familiaris'),
+(2, 'Cat', 'Felis catus');
+
+-- Insert initial data for dogs
+INSERT INTO pets (id, name, lastname, species_id, sex, breed, color, weight)
 VALUES 
-(1, 'Leia', 'Organa', 'female', 'Golden Retriever', ARRAY['Golden'], 42),
-(2, 'Totoño', 'Leopoldo', 'male', NULL, ARRAY['Light Yellow'], 10),
-(3, 'Guauguaucito', 'Feroz', 'female', 'Jack Russel', ARRAY['White', 'Brown'], 15)
+(1, 'Leia', 'Organa', 1, 'female', 'Golden Retriever', ARRAY['Golden'], 42),
+(2, 'Totoño', 'Leopoldo', 1, 'male', NULL, ARRAY['Light Yellow'], 10),
+(3, 'Guauguaucito', 'Feroz', 1, 'female', 'Jack Russel', ARRAY['White', 'Brown'], 15)
 ON CONFLICT (id) DO NOTHING;
 
--- Insert initial data into cats table
-INSERT INTO cats (id, name, lastname, sex, breed, color, weight)
+-- Insert initial data for cats
+INSERT INTO pets (id, name, lastname, species_id, sex, breed, color, weight)
 VALUES 
-(1, 'Sherlock', 'Holmes', 'male', 'American Shorthair', ARRAY['White', 'Orange'], 8.2),
-(2, 'Irene', 'Adler', 'female', 'American Shorthair', ARRAY['Black'], 4.7),
-(3, 'Mr. Mittens', 'Turbofreckles', 'male', 'Maine Coon', ARRAY['Gray'], 7.1)
+(4, 'Sherlock', 'Holmes', 2, 'male', 'American Shorthair', ARRAY['White', 'Orange'], 8.2),
+(5, 'Irene', 'Adler', 2, 'female', 'American Shorthair', ARRAY['Black'], 4.7),
+(6, 'Mr. Mittens', 'Turbofreckles', 2, 'male', 'Maine Coon', ARRAY['Gray'], 7.1)
 ON CONFLICT (id) DO NOTHING;
