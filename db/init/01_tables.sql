3edd0eb54fbed37c6cd0cdfb474cee9c3177785e
Federico Benavidez <fedoteh@gmail.com>
2024-09-25 16:35:30 -0300

    Add partial indexes for improved performance

--- db/init/01_tables.sql
+++ db/init/01_tables.sql
@@ -12,10 +12,14 @@ CREATE TABLE IF NOT EXISTS pets (
   sex VARCHAR(6) CHECK (sex IN ('male', 'female')) NOT NULL,
   breed VARCHAR(255),
   color VARCHAR(255)[] NOT NULL,
-  weight FLOAT NOT NULL,
+  weight FLOAT,
   profilePicture VARCHAR(255),
   activities VARCHAR(255)[],
   favoriteSnack VARCHAR(255),
   favoriteToy VARCHAR(255),
   favoritePlace VARCHAR(255)
 );
+
+-- We create the indexes to improve performance on queries for specific pets
+CREATE INDEX IF NOT EXISTS pets_dogs ON pets (species_id) WHERE species_id = 1;
+CREATE INDEX IF NOT EXISTS pets_cats ON pets (species_id) WHERE species_id = 2;
