71f863f96eb81a1ea866a94394c5f0570279fa37
Federico Benavidez <fedoteh@gmail.com>
2024-09-28 20:53:40 -0300

    Added API versioning

--- jest.config.js
+++ jest.config.js
@@ -5,4 +5,15 @@ module.exports = {
       '^.+\\.tsx?$': 'ts-jest',
     },
     moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  };
+    moduleNameMapper: {
+      '^@util/(.*)$': '<rootDir>/src/util/$1',
+      '^@routes/v1/(.*)$': '<rootDir>/src/routes/v1/$1',
+      '^@db_mock/(.*)$': '<rootDir>/src/db_mock/$1'
+    },
+    // Ensure Jest uses the same TypeScript configuration
+    transform: {
+      '^.+\\.tsx?$': ['ts-jest', { 
+        tsconfig: 'tsconfig.json' 
+      }],
+  },
+};
