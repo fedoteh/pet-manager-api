8d48797f85094fc9938d0923d7e0acd040114264
Federico Benavidez <fedoteh@gmail.com>
2024-10-15 15:35:33 -0300

    Sentry deployment

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,39 @@
+import "@util/monitoring/instrument";
+import * as dotenv from 'dotenv';
+import * as Sentry from "@sentry/node";
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import apiRouterV1 from './routes/v1/apiRouter';
 // import 'module-alias/register';
+
+// Load environment variables
+const envFile = process.env.NODE_ENV !== 'production' ? '.env.local' : '.env';
+dotenv.config({ path: envFile });
 
 // Setup express
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Setup routes
+app.use('/api/v1', apiRouterV1);
+app.get("/debug-sentry", function mainHandler(req, res) {
+    throw new Error("My first Sentry error!");
+});
 // Setup middleware libraries
+Sentry.setupExpressErrorHandler(app);
 app.use(cors());
 app.use(bodyParser.json());
 
-// Setup routes
-app.use('/api/v1', apiRouterV1);
+
+// Error handling middleware
+app.use(function onError(err: any, req: Request, res: Response, next: NextFunction) {
+    // The error id is attached to `res.sentry` to be returned
+    // and optionally displayed to the user for support.
+    res.statusCode = 500;
+    res.end((res as any).sentry + "\n");
+});
+
 
 if (process.env.NODE_ENV !== 'test') {
     app.listen(PORT, () => {
