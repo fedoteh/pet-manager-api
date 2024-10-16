9ae6c197c53df09da6fdeeba6c6eeb0b41e5bdbc
Federico Benavidez <fedoteh@gmail.com>
2024-10-16 13:23:31 -0300

    Fix sequelize on local runs with the prod image

--- src/util/db/sequelize.ts
+++ src/util/db/sequelize.ts
@@ -1,17 +1,31 @@
 import { Sequelize } from 'sequelize';
 
+const baseOptions = {
+  host: process.env.DB_HOST as string,
+  port: parseInt(process.env.DB_PORT || '5432', 10),
+  dialect: 'postgres',
+  logging: false,
+};
+
+const localOptions = process.env.NODE_ENV === 'local' ? {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // You might want to set this to true in production
+    },
+  },
+} : {};
+
+const sequelizeOptions = {
+  ...baseOptions,
+  ...localOptions,
+};
 
 const sequelize = new Sequelize(
   process.env.DB_NAME as string,
   process.env.DB_USER as string,
   process.env.DB_PASSWORD as string,
-  {
-    host: process.env.DB_HOST as string,
-    port: parseInt(process.env.DB_PORT || '5432', 10),
-    dialect: 'postgres',
-    logging: false,
-  }
+  sequelizeOptions as any,
 );
-
 
 export default sequelize;
